074e916ee0814a6f25faaab797cc2c252174d798
Rich Fecher <rfecher@gmail.com>
2016-08-25 09:27:12 -0400

    wip: testing build-docs

--- .utility/build-docs.sh
+++ .utility/build-docs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ "$TRAVIS_REPO_SLUG" == "ngageoint/geowave" ] && $BUILD_DOCS && [ "$TRAVIS_BRANCH" == "master" ]; then
+if [ "$TRAVIS_REPO_SLUG" == "ngageoint/geowave" ] && $BUILD_DOCS then
   echo -e "Building docs...\n"
-  mvn -P docs -pl docs install
+  mvn -e -X -P docs -pl docs install
   
-  echo -e "Building javadocs...\n"
-  mvn -q javadoc:aggregate
+  #echo -e "Building javadocs...\n"
+  #mvn -q javadoc:aggregate
 fi

a974e9cd40b7074988dfebb2aa6410fb98d7c4d5
Rich Fecher <rfecher@gmail.com>
2016-08-25 09:48:46 -0400

    wip

--- .utility/build-docs.sh
+++ .utility/build-docs.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-if [ "$TRAVIS_REPO_SLUG" == "ngageoint/geowave" ] && $BUILD_DOCS then
+if [ "$TRAVIS_REPO_SLUG" == "ngageoint/geowave" ] && $BUILD_DOCS; then
   echo -e "Building docs...\n"
   mvn -e -X -P docs -pl docs install
-  
-  #echo -e "Building javadocs...\n"
-  #mvn -q javadoc:aggregate
 fi
